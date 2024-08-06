e05807cc4d1dbf09750f6b5d21729f56454d50d7
CowBoys1st <draharjo9@gmail.com>
2024-08-06 19:56:40 +0700

    separo jalan

--- src/components/Camp.tsx
+++ src/components/Camp.tsx
@@ -11,7 +11,7 @@ interface CampProps {
 const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }: CampProps) => {
   return (
     <div className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}>
-      <div className="flex h-full flex-col items-start justify-between p-6 lg:px20 lg:py-10">
+      <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
         <div className="flexCenter gap-4">
           <div className="rounded-full bg-green-50 p-4">
             <Image 
@@ -50,19 +50,26 @@ const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }: CampProps)
 const Camp = () => {
   return (
     <section className='2xl:max-container relative flex flex-col py-10 lg:py-20 xl:mb-20'>
-      <div className="hide-scroolbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
         <CampSite
         backgroundImage="bg-bg-img-1" 
-        title="Putu truno camp"
+        title="Putuk truno camp"
         subtitle="Prigen, Pasuruan"
         peopleJoined="50+ Joined"
         />
         <CampSite
         backgroundImage="bg-bg-img-2" 
-        title="Putu truno camp"
-        subtitle="Prigen, Pasuruan"
+        title="Mountain View Camp"
+        subtitle="Somewhere in the wlderness"
         peopleJoined="50+ Joined"
         />
+      </div>
+
+      <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
+        <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
+          <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white"><strong>Feeling Lost</strong> And Not Knowing The Way?</h2>
+          <p className="regular-14 xl:regular-16 mt-5 text-white">Starting from the anxiety of the climbers when visiting a new climbing location, the possibility of getting lost is very large. That's why we are here for those of you who want to start an adventure</p>
+        </div>
       </div>
 
     </section>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -50,6 +50,8 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 };
 export default config;
